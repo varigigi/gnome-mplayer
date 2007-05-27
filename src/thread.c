7cb154b3d8415734a4dbfd97aaa72e44069ff98d
kdekorte <kdekorte@localhost>
2007-05-27 22:45:06 +0000

    Add -framedrop to the list of default options for mplayer

--- src/thread.c
+++ src/thread.c
@@ -416,6 +416,7 @@ gpointer launch_player(gpointer data)
     argv[arg++] = g_strdup_printf("-slave");
     argv[arg++] = g_strdup_printf("-identify");
     argv[arg++] = g_strdup_printf("-softvol");
+    argv[arg++] = g_strdup_printf("-framedrop");
     argv[arg++] = g_strdup_printf("-noconsolecontrols");
     argv[arg++] = g_strdup_printf("-osdlevel");
     argv[arg++] = g_strdup_printf("%i",osdlevel);
