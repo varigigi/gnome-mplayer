d9ba410ea87da5ccdf3a4118d356aeb9ce31cf99
kdekorte <kdekorte@localhost>
2010-12-15 15:55:04 +0000

    Fix detection of PA and GPM so errors are not emitted when not found

--- config.h
+++ config.h
@@ -119,7 +119,7 @@
 #define PACKAGE_VERSION ""
 
 /* Defined if you are using gnome-power-manager 2.26 or higher */
-#define SM_INHIBIT 1
+#define SM_INHIBIT 0
 
 /* Defined if you are using gnome-power-manager below 2.26 */
 #define SS_INHIBIT 0
