0b7be60a5ef859562ae6bb631e8e3f9c78c96a76
kdekorte <kdekorte@localhost>
2011-02-17 16:57:38 +0000

    libgmlib - gm_audio skeleton 	Fix typo in gnome-mplayer.schemas.in

--- src/libgmlib/gm_audio.c
+++ src/libgmlib/gm_audio.c
@@ -1,16 +1,16 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
- * gmlib.h
+ * gm_audio.c
  * Copyright (C) Kevin DeKorte 2006 <kdekorte@gmail.com>
  * 
- * gmlib.h is free software.
+ * gm_audio.c is free software.
  * 
  * You may redistribute it and/or modify it under the terms of the
  * GNU General Public License, as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option)
  * any later version.
  * 
- * gmlib.h is distributed in the hope that it will be useful,
+ * gm_audio.c is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -21,10 +21,3 @@
  * 	51 Franklin Street, Fifth Floor
  * 	Boston, MA  02110-1301, USA.
  */
-
-#include "gm_audio.h"
-#include "gm_file.h"
-#include "gm_parse.h"
-#include "gm_pref_store.h"
-#include "gm_strfuncs.h"
-
--- src/libgmlib/gm_audio.h
+++ src/libgmlib/gm_audio.h
@@ -1,16 +1,16 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
- * gmlib.h
+ * gm_audio.h
  * Copyright (C) Kevin DeKorte 2006 <kdekorte@gmail.com>
  * 
- * gmlib.h is free software.
+ * gm_audio.h is free software.
  * 
  * You may redistribute it and/or modify it under the terms of the
  * GNU General Public License, as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option)
  * any later version.
  * 
- * gmlib.h is distributed in the hope that it will be useful,
+ * gm_audio.h is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -22,9 +22,40 @@
  * 	Boston, MA  02110-1301, USA.
  */
 
-#include "gm_audio.h"
-#include "gm_file.h"
-#include "gm_parse.h"
-#include "gm_pref_store.h"
-#include "gm_strfuncs.h"
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 
+#ifndef __GM_AUDIO_H__
+#define __GM_AUDIO_H__
+
+typedef enum {
+    AUDIO_TYPE_SOFTVOL,	// use mplayer software volume control for non-alsa and non-pulse devices
+    AUDIO_TYPE_ALSA,
+    AUDIO_TYPE_PULSE
+} GmAudioType;
+
+typedef struct _AudioDevice {
+	gchar *description;    // use this as a key to look up the information 
+	GmAudioType type;
+	gint alsa_card;
+	gint alsa_device;
+	gchar *alsa_mixer;     // this is not looked up
+	gint pulse_index;
+	gchar *mplayer_ao;
+} AudioDevice;
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// prototypes go here
+
+
+	
+#ifdef __cplusplus
+}
+#endif
+
+#endif
