6de6dd8ff6fbd89acbee2d18e322c8083db5191e
kdekorte <kdekorte@localhost>
2011-04-21 16:51:41 +0000

    More configure macros

--- config.h
+++ config.h
@@ -40,6 +40,9 @@
 /* Define to 1 if you have GTK 2.22 or higher */
 #define GTK2_22_ENABLED 1
 
+/* Define to 1 if you have GTK 2.24 or higher */
+#define GTK2_24_ENABLED 1
+
 /* Define to 1 if you have GTK 3 or higher */
 /* #undef GTK3_ENABLED */
 
