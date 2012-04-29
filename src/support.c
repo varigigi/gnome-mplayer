6b190e19cb9c8d3d1a121a7a43a5f8c08151e611
kdekorte <kdekorte@localhost>
2012-04-29 18:40:04 +0000

    Fix Issue #613

--- src/support.c
+++ src/support.c
@@ -885,7 +885,7 @@ gint parse_vcd(gchar * filename)
 
         if (error_msg != NULL) {
             dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_ERROR,
-                                            GTK_BUTTONS_CLOSE, error_msg);
+                                            GTK_BUTTONS_CLOSE, "%s", error_msg);
             gtk_window_set_title(GTK_WINDOW(dialog), _("GNOME MPlayer Error"));
             gtk_dialog_run(GTK_DIALOG(dialog));
             gtk_widget_destroy(dialog);
